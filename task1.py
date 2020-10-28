74f065d5df951ffe865222885f0efce8dc43c84e
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-10-28 13:13:02 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -73,3 +73,43 @@ Buster dog
 Shih-tzu is owned by Christy
 (10 points) 
 """
+animals = []
+class pet:
+    animal = None
+    breed = None
+    Name = None
+    owner = None
+    birthdate = None
+
+    def __init__(self):
+        self.animal = input("What type of animal ")
+        self.breed = input("What is breed ")
+        self.name = input("What is animals name ")
+        self.owner = input("What is owners name ")
+        self.birthdate = input ("Animals date of birth ")
+            
+    
+
+
+
+
+# create a list of objects
+# while loop until they choose to exit
+# display menu and get menu choice
+# if 1 - insantiate a new object and add it to the list of objects and (fill in the data)
+# if 2 - ask for a name and then cycle through all of the list objects to see if the name matches and then print the name details
+# if 3 then quit
+while True:
+    print ("===================")
+    print ("1. Enter a new pet")
+    print ("2. Retrive a pet")
+    print ("3. Exit")
+    print ("===================")
+    choice = input("Enter number : ").strip()
+    if int(choice) == 1:
+        animals.append(pet())
+    elif int(choice) == 2:
+        animalName = (input("Enter animals name ")
+    else:
+        if int(choice) == 3:
+            break
